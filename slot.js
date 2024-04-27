ad105c2e170d703ce675044dd8b99d9770ce7de3
bejiinjaden <155380817+bejiinjaden@users.noreply.github.com>
2024-04-27 22:41:48 +0530

    Add files via upload

---
+++ slot.js
@@ -0,0 +1,25 @@
+const numoficons = 9,
+      speed = 100,
+      icon_height = 79.8;
+
+
+
+const roll = (reel , offset = 0) => {
+    const delta = (offset + 2) * numoficons + Math.round(Math.random() * numoficons);
+    const style = getComputedStyle(reel);
+    const backgroundPosition = parseFloat(style["background-position-y"]);
+
+    reel.style.transition = `background-position-y ${8 +delta*speed}ms`; 
+    reel.style.backgroundPositionY = `${backgroundPosition + delta * icon_height}px`;
+}
+
+
+function rollall(){
+    const reellist = document.querySelectorAll(' .slots > .reel');
+    [... reellist].map((reel,i) => {
+            console.log(reel,i);
+            roll(reel , i);
+    })
+}
+
+rollall();
